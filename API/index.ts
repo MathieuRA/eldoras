ad0e551f4b62091f776da84e738e5907da0a6fef
mathieu <contact@mathieu-raisin.fr>
2021-01-15 22:58:43 +0100

    remove useless code

--- API/index.ts
+++ API/index.ts
@@ -14,29 +14,6 @@ const PORT = process.env.PORT || 1251
 //app.use(express.static('public'))
 app.use(cors())
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, './public/uploads')
-//   },
-//   filename: function (req, file, cb) {
-//     cb(
-//       null,
-//       file.fieldname +
-//         '-' +
-//         Date.now() +
-//         path.extname(file.originalname)
-//     )
-//   },
-// })
-
-// app.post(
-//   '/test',
-//   upload.single('myImage'),
-//   (req, res, next) => {
-//     console.log(req['file'])
-//   }
-// )
-
 carsRoutes(app)
 
 app.listen(PORT, () => {
